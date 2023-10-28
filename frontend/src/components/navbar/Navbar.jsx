d7318e536effc1b1b1358a307dc487322532d078
Charles Proust <konoha33@hotmail.fr>
2023-10-28 14:45:10 +0200

    navbar v2

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/self-closing-comp */
+import "./navbar.scss";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="links">
+        <li>
+          <a href="home">Home</a>
+        </li>
+        <li>
+          <a href="infos">Infos</a>
+        </li>
+        <li>
+          <a href="contact">Contact</a>
+        </li>
+        <li>
+          <a href="login">Login</a>
+        </li>
+      </ul>
+      <div className="logo" alt="logo"></div>
+    </nav>
+  );
+}
+
+export default Navbar;
