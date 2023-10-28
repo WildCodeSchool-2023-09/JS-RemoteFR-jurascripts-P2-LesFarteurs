c80ab974136aaebf097c5fc3a5ce8e376813fbee
Charles Proust <konoha33@hotmail.fr>
2023-10-28 13:31:55 +0200

    page contact v1

---
+++ frontend/src/pages/contact/Contact.jsx
@@ -0,0 +1,9 @@
+import "./contact.scss";
+
+const Contact = () => {
+  return;
+  // eslint-disable-next-line no-unreachable
+  <div className="contact"> Contact</div>;
+};
+
+export default Contact;
