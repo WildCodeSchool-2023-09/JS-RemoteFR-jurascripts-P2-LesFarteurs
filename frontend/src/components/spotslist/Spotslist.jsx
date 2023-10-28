3d2c7e8742596b1f275e2aea57972d632771428c
Charles Proust <konoha33@hotmail.fr>
2023-10-28 15:12:19 +0200

    creation spotslist

---
+++ frontend/src/components/spotslist/Spotslist.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/function-component-definition */
+import "./spotslist.scss";
+
+const Spotslist = () => {
+  return (
+    <div id="SpotslistContainer">
+      <h1>Spotslist</h1>
+
+      <p>Spotslist Page</p>
+    </div>
+  );
+};
+
+export default Spotslist;
