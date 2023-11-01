2b5e4111f7dbde8fc2dfecebdee9cfadc47c6750
CASSAR SUZY <contact@scassar.com>
2023-11-01 13:46:56 +0100

    Modif NaBar et Header pour du responsive

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -3,28 +3,19 @@ import "./_navbar.scss";
 
 function Navbar() {
   return (
-    <section>
-      <nav className="navbar">
-        <ul className="links">
-          <li>
-            <a href="home">Home</a>
-          </li>
-          <li>
-            <a href="infos">Infos et définitions</a>
-          </li>
-          <li>
-            <a href="contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <div className="mask">
-        <img
-          className="bandeau"
-          src="./src/assets/Bandeau-men-surfing2.jpg"
-          alt="bandeau"
-        />
-      </div>
-    </section>
+    <nav className="navbar">
+      <ul className="links">
+        <li>
+          <a href="home">Home</a>
+        </li>
+        <li>
+          <a href="infos">Infos et définitions</a>
+        </li>
+        <li>
+          <a href="contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
