f404c854cb5b8fdc3400b5f253949c19873a2d6f
Charles Proust <konoha33@hotmail.fr>
2023-11-16 22:34:08 +0100

    ajustement css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import Coolitude from "./components/coolitude/Coolitude";
 import Header from "./components/header/Header";
 import Alert from "./components/alert/Alert";
@@ -9,41 +8,14 @@ import Footer from "./components/footer/Footer";
 import "./styles/index.css";
 
 function App() {
-  const [smallScreen, setSmallScreen] = useState(window.innerWidth <= 768);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setSmallScreen(window.innerWidth <= 768);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <div className="app">
-      {smallScreen ? (
-        <div>
-          <Navbar />
-          <Header />
-          <Alert />
-          <Algo />
-          <Coolitude />
-          <Footer />
-        </div>
-      ) : (
-        <div>
-          <Coolitude />
-          <Navbar />
-          <Header />
-          <Alert />
-          <Algo />
-          <Footer />
-        </div>
-      )}
+      <Coolitude />
+      <Navbar />
+      <Header />
+      <Alert />
+      <Algo />
+      <Footer />
     </div>
   );
 }
